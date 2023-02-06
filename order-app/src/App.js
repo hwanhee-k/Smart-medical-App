bdaccdb68d747438ff47509a252fd8d643b7269d
kim hwanhee <>
2023-02-06 16:04:16 +0900

    .

--- order-app/src/App.js
+++ order-app/src/App.js
@@ -3,18 +3,20 @@ import DraggableList from "./component/DraggableList";
 import { Routes, Route } from "react-router-dom";
 import ReceptionPage from "./page/DetailPage/ReceptionPage";
 import MainPage from "./page/MainPage";
+import Layout from "./component/common/Layout";
 
 function App() {
   return (
     <div className="App">
-      <Routes>
+      {/* <Routes>
         <Route exact path="/" element={<MainPage />} />
         <Route
           exact
           path="/reception/:content/:name"
           element={<ReceptionPage />}
         />
-      </Routes>
+      </Routes> */}
+      <Layout />
     </div>
   );
 }
---
+++ order-app/src/component/common/Layout.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./Header";
+import styled from "styled-components";
+
+const Layout = () => {
+  return (
+    <div>
+      <Header></Header>
+      <div>1111111111</div>
+    </div>
+  );
+};
+
+export default Layout;
+const contentContainer = styled.div`
+  padding: 100px;
+`;
