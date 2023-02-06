9462bd0906cfae8985850c868821ce6f3e7716bc
kim hwanhee <>
2023-02-06 16:37:24 +0900

    레이아웃 컴포넌트 구성

--- order-app/src/component/common/Layout.js
+++ order-app/src/component/common/Layout.js
@@ -1,17 +1,23 @@
 import React from "react";
 import Header from "./Header";
 import styled from "styled-components";
+import { Toolbar } from "@mui/material";
+import { useSlotProps } from "@mui/base";
 
-const Layout = () => {
+const Layout = (props) => {
   return (
     <div>
-      <Header></Header>
-      <div>1111111111</div>
+      <Header />
+      <Toolbar />
+      <ContentContainer>
+        <main>{props.children}</main>
+      </ContentContainer>
     </div>
   );
 };
 
 export default Layout;
-const contentContainer = styled.div`
-  padding: 100px;
+const ContentContainer = styled.div`
+  border: 1px solid brown;
+  background-color: azure;
 `;
