f50cd932828bb6058118a7f7da606830464e8c42
kim hwanhee <>
2023-02-06 17:48:49 +0900

    레이아웃 연결한 MainPage 라우팅

--- order-app/src/App.js
+++ order-app/src/App.js
@@ -1,22 +1,17 @@
-import "./App.css";
-import DraggableList from "./component/DraggableList";
 import { Routes, Route } from "react-router-dom";
-import ReceptionPage from "./page/DetailPage/ReceptionPage";
 import MainPage from "./page/MainPage";
-import Layout from "./component/common/Layout";
 
 function App() {
   return (
     <div className="App">
-      {/* <Routes>
+      <Routes>
         <Route exact path="/" element={<MainPage />} />
-        <Route
+        {/* <Route
           exact
           path="/reception/:content/:name"
           element={<ReceptionPage />}
-        />
-      </Routes> */}
-      <Layout />
+        /> */}
+      </Routes>
     </div>
   );
 }
