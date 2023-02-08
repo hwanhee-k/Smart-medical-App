4db5f8cbf4503afa0886de4cb80602ec728ca217
JoyKim66 <masscom6051@naver.com>
2023-02-09 00:23:23 +0900

    onDragEnd함수 수정중

--- order-app/src/component/MainPage.js
+++ order-app/src/component/MainPage.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import CustomAccordion from "./CustomAccordion";
 
-import DraggableList, { NestedDnd } from "./DraggableList";
+import DraggableList from "./DraggableList";
 
 const MainPage = ({ children }) => {
   const todo = ["1", "2", "3", "4"];
@@ -15,6 +15,13 @@ const MainPage = ({ children }) => {
     if (!result.destination) {
       return;
     }
+    if (nestedState === patientName) {
+      setNestedState(patientName);
+    } else if (nestedState === RECEPTION_ORDER_LIST) {
+      setNestedState(RECEPTION_ORDER_LIST);
+    } else {
+      setNestedState(todo);
+    }
 
     const originData = [...nestedState];
     const [reorderedData] = originData.splice(result.source.index, 1);
@@ -24,15 +31,6 @@ const MainPage = ({ children }) => {
     console.log(originData);
   };
 
-  useEffect(() => {
-    if (nestedState === patientName) {
-      setNestedState(patientName);
-    } else if (nestedState === RECEPTION_ORDER_LIST) {
-      setNestedState(RECEPTION_ORDER_LIST);
-    } else {
-      setNestedState(todo);
-    }
-  }, []);
 
   return (
     <DraggableList
